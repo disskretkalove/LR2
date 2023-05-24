8df2334884b11dc3deab0666567747796c6f0a57
disskretkalove <96077142+disskretkalove@users.noreply.github.com>
2023-05-25 00:46:57 +0300

    Add files via upload

---
+++ LR2-main/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+// mega cringe code, aaaaahahahahahahah, sup
+// new comment
+int main()
+{
+    std::string name;
+    std::cout << "Please enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from @" << name << std::endl;
+    return 0;
+}
